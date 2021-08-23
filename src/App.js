fe55b9fa7a60385932bb4c42f92a7eb3112d7e68
alexis-forbes <a.forbes.gil@gmail.com>
2021-08-23 16:36:35 +0200

    user_age_app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, Fragment } from 'react';
+
+import AddUser from './Components/Users/AddUser';
+import UsersList from './Components/Users/UsersList';
+
+function App() {
+
+  const [usersList, setUsersList] = useState([]); 
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, { name: uName, age:uAge, id: Math.random().toString() }]; 
+    }); 
+  }
+
+
+  return (
+    <Fragment>
+      <AddUser onAddUser={addUserHandler}></AddUser>
+      <UsersList users={[usersList]}></UsersList>
+    </Fragment>
+  );
+}
+
+export default App;
